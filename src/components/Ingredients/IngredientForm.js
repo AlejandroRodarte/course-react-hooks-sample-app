cdfc700b25d5392c0d1b761844c34a7f2fd9e98c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-07-07 11:32:01 -0600

    Getting Started with `useState()` and More on `useState()` & State Updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -5,6 +5,11 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
 
+  const [formState, setFormState] = useState({
+    title: '',
+    amount: ''
+  });
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -12,21 +17,43 @@ const IngredientForm = React.memo(props => {
 
   return (
     <section className="ingredient-form">
+
       <Card>
+
         <form onSubmit={submitHandler}>
+
           <div className="form-control">
+
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+
+            <input type="text" id="title" value={ formState.title } onChange={ e => {
+              const newTitle = e.target.value;
+              setFormState((prevState) => ({ title: newTitle, amount: prevState.amount }))
+            } } />
+
           </div>
+
           <div className="form-control">
+
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+
+            <input type="number" id="amount" value={ formState.amount } onChange={ e => {
+              const newAmount = e.target.value;
+              setFormState((prevState) => ({ title: prevState.title, amount: newAmount }));
+            } } />
+
           </div>
+
           <div className="ingredient-form__actions">
+
             <button type="submit">Add Ingredient</button>
+
           </div>
+
         </form>
+
       </Card>
+
     </section>
   );
 });
