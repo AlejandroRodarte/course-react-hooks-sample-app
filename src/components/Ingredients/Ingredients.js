2d249a689c6be59599fa2f6e7d056b82636f495b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-07-10 12:32:25 -0600

    Sending Http Requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,24 @@ const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => setIngredients((prevIngredients) => [...prevIngredients, {
-    id: Math.random().toString(),
-    ...ingredient
-  }]);
+  const addIngredientHandler = async ingredient => {
+
+    const data = await fetch('https://react-ingredients-app.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const json = await data.json();
+
+    setIngredients((prevIngredients) => [...prevIngredients, {
+      id: json.name,
+      ...ingredient
+    }]);
+
+  };
 
   return (
     <div className="App">
